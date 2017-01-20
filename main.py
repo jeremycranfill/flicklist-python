4d9cd3baa940db57717692c661b9f9b9fe375338
JeremyC <jeremy.cranfill@gmail.com>
2017-01-19 20:16:40 -0600

    i did the thing to the thing

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 import webapp2
+import random
 
 class Index(webapp2.RequestHandler):
 
     def getRandomMovie(self):
 
-        # TODO: make a list with at least 5 movie titles
-
-        # TODO: randomly choose one of the movies, and return it
-
-        return "The Big Lebowski"
+        movieList=['Decker','The Phantom menace', 'Anime', 'Cool', 'Bad']
+        randMovie = random.choice(movieList)
+        return randMovie
 
     def get(self):
         # choose a movie by invoking our new function
         movie = self.getRandomMovie()
+        movie2 = self.getRandomMovie()
 
         # build the response string
         content = "<h1>Movie of the Day</h1>"
         content += "<p>" + movie + "</p>"
+        content += "<h1>Movie of Tommorrow</h1>"
+        content += "<p>" + movie2 + "</p>"
 
         # TODO: pick a different random movie, and display it under
         # the heading "<h1>Tommorrow's Movie</h1>"
